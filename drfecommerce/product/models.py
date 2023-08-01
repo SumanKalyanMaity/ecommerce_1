4e059386782cfe54cb934d23a1f11f57dc6662df
suman <kalyansuman62@gmail.com>
2023-08-02 02:30:04 +0530

    small ecommerce project

---
+++ drfecommerce/product/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, blank = True, null = True)
+    
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+        
+    def __str__(self):
+        return self.name
+    
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey("Category", null = True, blank = True, on_delete = models.SET_NULL)
+    
+    def __str__(self):
+        return self.name
